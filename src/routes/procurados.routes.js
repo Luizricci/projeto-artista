608861f313adf6aabfbfea6d5c8dbc7a12ef42ef
Luiz Aureliano <luizaureliano.tds1.senai@gmail.com>
2024-10-10 10:47:36 -0300

    Feat: create put method

--- src/routes/procurados.routes.js
+++ src/routes/procurados.routes.js
@@ -2,72 +2,117 @@ import { response, Router } from "express";
 const procuradosRoutes = Router();
 
 let artistas = [
-{
-    id: 1,
+  {
+    id: Math.floor(Math.random() * 1000000),
     nome: "Pitty",
-    descricaoFisica: ["cabelos curtos", "olhos pretos", "cavanhaque", "tamanho baixo"],
-    envolvimentosSuspeito: true,
-},
+    descricaoFisica: [
+      "cabelos curtos",
+      "olhos pretos",
+      "cavanhaque",
+      "tamanho baixo",
+    ],
+    envolvimentosSuspeito: "sim",
+  },
+  {
+    id: Math.floor(Math.random() * 1000000),
+    nome: "michael jackson",
+    descricaoFisica: ["cabelos longos", "olhos pretos", "tamanho alto"],
+    envolvimentosSuspeito: "sim",
+  },
 ];
 
 // Rota para listar todos os artistas Procurados
 procuradosRoutes.get("/", (req, res) => {
-    return res.status(200).json(artistas);
+  return res.status(200).json(artistas);
 });
 
-// Rota para cadastrar um novo planeta
+// Rota para cadastrar um novo artista
 procuradosRoutes.post("/", (req, res) => {
-    const { nome, descricaoFisica, envolvimentosSuspeito} = req.body;
-    
-      // Validação do campo obrigatório
-    if (!nome) {
-        return res.status(400).json({
-        message: "O campo nome é obrigatório!",
-        });
-    }
-    
-      // Validação de envolvimentosSuspeito (sim ou não)
-    if (envolvimentosSuspeito != "sim" && envolvimentosSuspeito != "não") {
-        return res.status(400).send({
-            message: "Digite 'sim' ou 'não'!",
-        });
-    }
+  const { nome, descricaoFisica, envolvimentosSuspeito } = req.body;
 
-    // Criação de um novo artista
-const novoArtista = {
+  // Validação do campo obrigatório
+  if (!nome) {
+    return res.status(400).json({
+      message: "O campo nome é obrigatório!",
+    });
+  }
+
+  // Validação de envolvimentosSuspeito (sim ou não)
+  if (envolvimentosSuspeito != "sim" && envolvimentosSuspeito != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
+
+  // Criação de um novo artista
+  const novoArtista = {
     id: Math.floor(Math.random() * 1000000),
     nome,
     descricaoFisica: descricaoFisica || [], // Valor padrão caso descricaoFisica não seja enviado
     envolvimentosSuspeito,
-};
+  };
 
   // Adiciona o novo Artista ao array de artistas
-artistas.push(novoArtista);
+  artistas.push(novoArtista);
 
-return res.status(201).json({
+  return res.status(201).json({
     message: "Artista cadastrado com sucesso!",
     novoArtista,
-});
+  });
 });
 
-// Rota para buscar um planeta pelo id
+// Rota para buscar um artista pelo id
 procuradosRoutes.get("/:id", (req, res) => {
-    const { id } = req.params;
-    
-      // Busca um planeta pelo id no array de planetas
-    const artista = artistas.find((planet) => planet.id == id);
-    
-      // Verifica se o planeta foi encontrado
-    if (!artista) {
-        return res
-            .status(404)
-            .json({ message: `artista com id ${id} não encontrado!` });
-    }
-    
-        return res.status(200).json(artista);
+  const { id } = req.params;
+
+  // Busca um artista pelo id no array de artista
+  const artista = artistas.find((rapper) => rapper.id == id);
+
+  // Verifica se o artista foi encontrado
+  if (!artista) {
+    return res
+      .status(404)
+      .json({ message: `artista com id ${id} não encontrado!` });
+  }
+
+  return res.status(200).json(artista);
+});
+
+// Rota para atualizar um artista pelo id
+procuradosRoutes.put("/:id", (req, res) => {
+  const { id } = req.params;
+
+  //console.log(id);
+  
+  const { nome, descricaoFisica, envolvimentosSuspeito } = req.body;
+
+  console.log(nome, descricaoFisica, envolvimentosSuspeito);
+
+  // Busca um artista pelo id no array de artista
+  const artista = artistas.find((rapper) => rapper.id == id);
+
+  // Validação do campo obrigatório
+  if (!nome) {
+    return res.status(400).json({
+      message: "O campo nome é obrigatório!",
     });
+  }
 
+  // Validação de envolvimentosSuspeito (sim ou não     )
+  if (envolvimentosSuspeito != "sim" && envolvimentosSuspeito != "não") {
+    return res.status(400).send({
+      message: "Digite 'sim' ou 'não'!",
+    });
+  }
 
+  artista.nome = nome;
+  artista.descricaoFisica = descricaoFisica;
+  artista.envolvimentosSuspeito = envolvimentosSuspeito;
 
+  return res.status(200).json({
+    message: "artista atualizado com sucesso!",
+    artista,
+  });
+});
 
 export default procuradosRoutes;
